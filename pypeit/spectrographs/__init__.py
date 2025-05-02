0d64b7e5ca24974ae2ddee79e2ceb652f3e56fbc
AJ WONG <165871292+aldric98@users.noreply.github.com>
2025-05-02 11:32:09 +0100

    Update __init__.py

--- pypeit/spectrographs/__init__.py
+++ pypeit/spectrographs/__init__.py
@@ -34,7 +34,6 @@ from pypeit.spectrographs import shane_kast
 from pypeit.spectrographs import soar_goodman
 from pypeit.spectrographs import tng_dolores
 from pypeit.spectrographs import vlt_fors
-from pypeit.spectrographs import vlt_uves
 from pypeit.spectrographs import vlt_xshooter
 from pypeit.spectrographs import vlt_sinfoni
 from pypeit.spectrographs import wht_isis
